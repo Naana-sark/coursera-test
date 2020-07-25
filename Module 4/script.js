daa1ae7ff279423794b12befbcae787d7ee07a47
Naana-sark <67606353+Naana-sark@users.noreply.github.com>
2020-07-25 12:55:47 +0000

    Update script.js

--- Module 4/script.js
+++ Module 4/script.js
@@ -1,5 +1,9 @@
 (function (window) {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  var byeSpeaker = {
+    speak: function (name) {
+      console.log(speakWord + " " + name);
+    }
 
   for (var i in names) {
     var firstLetter = names[i].charAt(0).toLowerCase();
