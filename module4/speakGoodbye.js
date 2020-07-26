bf565d84db5b66d13ecb3d7a2ae61205cb520b8e
Naana-sark <67606353+Naana-sark@users.noreply.github.com>
2020-07-26 16:49:49 +0000

    Update speakGoodbye.js

--- module4/speakGoodbye.js
+++ module4/speakGoodbye.js
@@ -1,9 +1,8 @@
 (function (window) {
+  var byeSpeaker = {};
   var speakWord = "Good Bye";
-  var byeSpeaker = {
-    speak: function (name) {
-      console.log(speakWord + " " + name);
-    }
-  };
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
   window.byeSpeaker = byeSpeaker;
 })(window);
